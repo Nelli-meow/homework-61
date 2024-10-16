fcd72edf5bbb5a2ab466d05871da2640f8bdf9e4
Nelli Gorlova <nelli7035@gmail.com>
2024-10-17 04:24:34 +0600

    EX61 is done

--- src/components/Containers/Containers.tsx
+++ src/components/Containers/Containers.tsx
@@ -1,14 +1,17 @@
 import Countries from '../Countries/Countries.tsx';
 import CountryInfo from '../CountryInfo/CountryInfo.tsx';
+import { useState } from 'react';
 
 const Containers = () => {
+  const [countryInfo, setCountryInfo] = useState(null);
+
   return (
     <div className="row border border-3">
       <div className="overflow-auto col-4" style={{maxHeight: '600px'}}>
-        <Countries/>
+        <Countries setCountryInfo={setCountryInfo}/>
       </div>
       <div className="col">
-        <CountryInfo/>
+        <CountryInfo country={countryInfo} />
       </div>
     </div>
   );
